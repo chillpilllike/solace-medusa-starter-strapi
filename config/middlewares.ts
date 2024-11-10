bf43dbbf78c65c885a0d122059a681d15df29a5e
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-10 10:52:47 +0530

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -28,6 +28,15 @@ export default [
     },
   },
   "strapi::cors",
+{
+  name: "strapi::cors",
+  config: {
+    enabled: true,
+    origin: ["*"], // Use "*" to allow all or specify domains e.g., ["http://localhost:3000"]
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    headers: ["Content-Type", "Authorization"],
+  },
+},
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
